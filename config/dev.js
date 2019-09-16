02b3577007ef55602442aeb34614f20e7a9ce618
Keeler Mendoza <keelerbe@gmail.com>
2019-09-16 18:43:00 +0100

    set up passport-oauth

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '562765985987-c6cjll1uljv0ndokng2ejmciti11p08p.apps.googleusercontent.com',
+  googleClientSecret: '7G8yod_imsfA69NtH3DZ2Ggb',
+  mongoURI: 'mongodb://keeler:password123@ds127854.mlab.com:27854/psprt-oauth-dev',
+  cookieKey: 'iHeartCookies'
+}
--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require('express')
+const passport = require('passport')
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const bodyParser = require('body-parser')
+require('./models/userSchema')
+require('./services/passport')
 require('colors')
+const keys = require('./config/keys')
 
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+})
 const app = express()
+
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [keys.cookieKey]
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.send({ success: true }))
 
